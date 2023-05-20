c42251aee3f9985caf13de6056493f406655bbd3
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2023-05-19 22:43:03 -0400

    nostr-async/nostr-async/main.c: add header relative paths

--- nostr-async/nostr-async/main.c
+++ nostr-async/nostr-async/main.c
@@ -6,9 +6,24 @@
 //
 
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <errno.h>
+#include <inttypes.h>
+#include <unistd.h>
+
 #include "secp256k1.h"
 #include "secp256k1_ecdh.h"
 #include "secp256k1_schnorrsig.h"
+
+#include "../../cursor.h"
+#include "../../hex.h"
+#include "../../base64.h"
+#include "../../aes.h"
+#include "../../sha256.h"
+#include "../../random.h"
+#include "../../proof.h"
 
 int main(int argc, const char * argv[]) {
     // insert code here...

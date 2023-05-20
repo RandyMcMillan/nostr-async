78713ac9803ed1561284374a98637d5ba7a92c67
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2023-05-19 21:55:35 -0400

    nostr-async.xcworkspace: add

---
+++ nostr-async/nostr-async/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  nostr-async
+//
+//  Created by git on 5/19/23.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
